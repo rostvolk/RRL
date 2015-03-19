7289353849fcfd1ebb427f1810ff3990ea7937aa
rostvolk <rostvolk@gmail.com>
2015-03-19 15:06:25 +0300

    modify structure mysql

--- mysql/RRLTraffic.sql
+++ mysql/RRLTraffic.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: localhost
--- Время создания: Мар 16 2015 г., 17:30
+-- Время создания: Мар 19 2015 г., 15:04
 -- Версия сервера: 5.5.41-0ubuntu0.14.04.1
 -- Версия PHP: 5.5.9-1ubuntu4.6
 
@@ -31,6 +31,8 @@ CREATE TABLE IF NOT EXISTS `host` (
   `host_type` mediumint(8) unsigned NOT NULL DEFAULT '0',
   `description` varchar(150) NOT NULL DEFAULT '',
   `hostname` varchar(250) DEFAULT NULL,
+  `IndexPoint` int(5) NOT NULL,
+  `addres` varchar(150) NOT NULL,
   `notes` text,
   `snmp_community` varchar(100) DEFAULT NULL,
   `snmp_version` tinyint(1) unsigned NOT NULL DEFAULT '3',
@@ -79,6 +81,30 @@ CREATE TABLE IF NOT EXISTS `Interface` (
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `marks`
+--
+
+CREATE TABLE IF NOT EXISTS `marks` (
+  `id` int(3) DEFAULT NULL,
+  `name` int(4) DEFAULT NULL,
+  `descr` varchar(267) DEFAULT NULL,
+  `scale1` int(2) DEFAULT NULL,
+  `scale2` int(2) DEFAULT NULL,
+  `lonlatarr` varchar(32) DEFAULT NULL,
+  `lonL` varchar(11) DEFAULT NULL,
+  `latT` varchar(11) DEFAULT NULL,
+  `LonR` varchar(11) DEFAULT NULL,
+  `LatB` varchar(11) DEFAULT NULL,
+  `color1` int(11) DEFAULT NULL,
+  `color2` int(11) DEFAULT NULL,
+  `visible` varchar(4) DEFAULT NULL,
+  `picname` varchar(15) DEFAULT NULL,
+  `categoryid` int(1) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `SNMP-Traffic`
 --
 
@@ -104,7 +130,7 @@ CREATE TABLE IF NOT EXISTS `SNMP-Traffic` (
   KEY `SNMP_unix_time` (`SNMP_unix_time`),
   KEY `SNMP_date` (`SNMP_date`),
   KEY `fk_SNMP-Traffic_Interface1_idx` (`Interface_id`,`Interface_host_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=koi8r COMMENT='Ð¢Ñ€Ð°Ñ„Ñ„Ð¸Ðº Ð¿Ð¾ SNMP' AUTO_INCREMENT=229361 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=koi8r COMMENT='Ð¢Ñ€Ð°Ñ„Ñ„Ð¸Ðº Ð¿Ð¾ SNMP' AUTO_INCREMENT=434172 ;
 
 -- --------------------------------------------------------
 
@@ -186,6 +212,40 @@ CREATE TABLE IF NOT EXISTS `Traf_group_day_full` (
 -- --------------------------------------------------------
 
 --
+-- Дублирующая структура для представления `view_metric`
+--
+CREATE TABLE IF NOT EXISTS `view_metric` (
+`Metric` varchar(349)
+,`SNMP_unix_time` int(32)
+,`IFspeed` int(9)
+,`interval_measure` int(32)
+,`InHighOctets` bigint(255)
+,`OutHighOctets` bigint(255)
+,`InDiscards` bigint(255)
+,`OutDiscards` bigint(255)
+,`InErrors` bigint(255)
+,`OutErrors` bigint(255)
+);
+-- --------------------------------------------------------
+
+--
+-- Дублирующая структура для представления `View_Metric_last10min`
+--
+CREATE TABLE IF NOT EXISTS `View_Metric_last10min` (
+`Metric` varchar(349)
+,`SNMP_unix_time` int(32)
+,`IFspeed` int(9)
+,`interval_measure` int(32)
+,`InHighOctets` bigint(255)
+,`OutHighOctets` bigint(255)
+,`InDiscards` bigint(255)
+,`OutDiscards` bigint(255)
+,`InErrors` bigint(255)
+,`OutErrors` bigint(255)
+);
+-- --------------------------------------------------------
+
+--
 -- Структура для представления `Traff`
 --
 DROP TABLE IF EXISTS `Traff`;
@@ -209,6 +269,24 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`rost`@`192.168.100.214` SQL SECURITY DEFINER
 DROP TABLE IF EXISTS `Traf_group_day_full`;
 
 CREATE ALGORITHM=UNDEFINED DEFINER=`rost`@`192.168.100.214` SQL SECURITY DEFINER VIEW `Traf_group_day_full` AS select `a`.`id` AS `id`,`a`.`host_id` AS `host_id`,`a`.`snmp_date` AS `snmp_date`,`a`.`max_trafIN_Mb` AS `max_trafIN_Mb`,`a`.`avg_trafIN_Mb` AS `avg_trafIN_Mb`,`a`.`max_UtilIN_proc` AS `max_UtilIN_proc`,`a`.`max_trafout_Mb` AS `max_trafout_Mb`,`a`.`avg_trafout_Mb` AS `avg_trafout_Mb`,`a`.`max_Utilout_proc` AS `max_Utilout_proc`,round((`b`.`ifSpeed` / 1000000),2) AS `ifspeedMb`,`b`.`hostname` AS `hostname`,`b`.`IfName` AS `Ifname`,`b`.`ifSpeed` AS `ifspeed`,`b`.`ifDescr` AS `ifDescr` from (`Traff_group_by_day` `a` join `Interface` `b`) where (`a`.`id` = `b`.`id`);
+
+-- --------------------------------------------------------
+
+--
+-- Структура для представления `view_metric`
+--
+DROP TABLE IF EXISTS `view_metric`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`rost`@`192.168.100.214` SQL SECURITY DEFINER VIEW `view_metric` AS select concat_ws('.',`c`.`description`,`b`.`IfTypeName`,concat('(',replace(`b`.`IfName`,'/','-'),')',`b`.`ifDescr`)) AS `Metric`,`a`.`SNMP_unix_time` AS `SNMP_unix_time`,`a`.`IFspeed` AS `IFspeed`,`a`.`interval_measure` AS `interval_measure`,`a`.`InHighOctets` AS `InHighOctets`,`a`.`OutHighOctets` AS `OutHighOctets`,`a`.`InDiscards` AS `InDiscards`,`a`.`OutDiscards` AS `OutDiscards`,`a`.`InErrors` AS `InErrors`,`a`.`OutErrors` AS `OutErrors` from ((`SNMP-Traffic` `a` join `Interface` `b`) join `host` `c`) where ((`a`.`Interface_id` = `b`.`id`) and (`a`.`Interface_host_id` = `c`.`id`) and (`c`.`description` like 'Yar%'));
+
+-- --------------------------------------------------------
+
+--
+-- Структура для представления `View_Metric_last10min`
+--
+DROP TABLE IF EXISTS `View_Metric_last10min`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`rost`@`192.168.100.214` SQL SECURITY DEFINER VIEW `View_Metric_last10min` AS select concat_ws('.',`c`.`description`,`b`.`IfTypeName`,concat('<',replace(`b`.`IfName`,'/','-'),'>',`b`.`ifDescr`)) AS `Metric`,`a`.`SNMP_unix_time` AS `SNMP_unix_time`,`a`.`IFspeed` AS `IFspeed`,`a`.`interval_measure` AS `interval_measure`,`a`.`InHighOctets` AS `InHighOctets`,`a`.`OutHighOctets` AS `OutHighOctets`,`a`.`InDiscards` AS `InDiscards`,`a`.`OutDiscards` AS `OutDiscards`,`a`.`InErrors` AS `InErrors`,`a`.`OutErrors` AS `OutErrors` from ((`SNMP-Traffic` `a` join `Interface` `b`) join `host` `c`) where ((`a`.`Interface_id` = `b`.`id`) and (`a`.`Interface_host_id` = `c`.`id`) and (`c`.`description` like 'Yar-%') and ((unix_timestamp() - `a`.`SNMP_unix_time`) < 600));
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
